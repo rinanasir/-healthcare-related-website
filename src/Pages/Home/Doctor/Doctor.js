35435f4230a4b287f3673581f38a1deff17f9f27
rinanasir <rinanasir25@gmail.com>
2021-10-19 17:00:51 +0600

    added doctor

--- src/Pages/Home/Doctor/Doctor.js
+++ src/Pages/Home/Doctor/Doctor.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Doctor = () => {
+const Doctor = ({ doctor }) => {
+    const { name, about, image } = doctor
     return (
-        <div>
-            <h2>This is doctors</h2>
+        <div className="col-lg-3 col-sm-6 col-12 bg-color">
+            <img className="img-fluid rounded-3 pt-4" src={image} alt="" />
+            <h3 className="text-secondary fw-bold">{name}</h3>
+            <p className="text-danger fw-bold">{about}</p>
         </div>
     );
 };
--- src/Pages/Home/Doctors/Doctors.js
+++ src/Pages/Home/Doctors/Doctors.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Doctor from '../Doctor/Doctor';
 
 const Doctors = () => {
     const [doctors, setDoctors] = useState([]);
@@ -9,7 +10,15 @@ const Doctors = () => {
     }, []);
     return (
         <div>
-
+            <h1 className="text-primary fw-bold mt-4 pt-4">Meet Our Doctors</h1>
+            <div className="row mx-5 px-5">
+                {
+                    doctors.map(doctor => <Doctor
+                        key={doctor.key}
+                        doctor={doctor}
+                    ></Doctor>)
+                }
+            </div>
         </div>
     );
 };
